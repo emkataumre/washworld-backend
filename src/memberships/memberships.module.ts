06321ecce5dfd0f9e2fe318086523580f596cef8
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2024-05-20 21:28:51 +0200

    add imports

--- src/memberships/memberships.module.ts
+++ src/memberships/memberships.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MembershipsService } from './memberships.service';
 import { MembershipsController } from './memberships.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Membership } from './entities/membership.entity';
 
 @Module({
   controllers: [MembershipsController],
   providers: [MembershipsService],
+  imports: [TypeOrmModule.forFeature([Membership])],
 })
 export class MembershipsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MembershipsModule } from 'src/memberships/memberships.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { CarsModule } from 'src/cars/cars.module';
+import { LocationsModule } from 'src/locations/locations.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => MembershipsModule),
+    forwardRef(() => LocationsModule),
+    forwardRef(() => CarsModule),
+  ],
 })
 export class UsersModule {}
