49015560ca7dcc9911d05e0e2ce3c1f6a0511fda
emkataumre <emo.vladinov@gmail.com>
2024-05-24 23:27:38 +0200

    refactor: update status service to include update + controller endpoint

--- src/statuses/statuses.controller.ts
+++ src/statuses/statuses.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { StatusesService } from './statuses.service';
+import { Status } from './entities/status.entity';
 
 @Controller('statuses')
 export class StatusesController {
@@ -13,5 +14,13 @@ export class StatusesController {
   @Get()
   findAllStatusesWithHalls() {
     return this.statusesService.findAllStatusesWithHalls();
+  }
+
+  @Put(':hall_id')
+  updateStatus(
+    @Param('hall_id') hall_id: number,
+    @Body() newStatus: Partial<Status>,
+  ) {
+    return this.statusesService.updateStatus(hall_id, newStatus);
   }
 }
--- src/statuses/statuses.service.ts
+++ src/statuses/statuses.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Status } from './entities/status.entity';
 import { HallsService } from 'src/halls/halls.service';
+import { Hall } from 'src/halls/entities/hall.entity';
 
 @Injectable()
 export class StatusesService {
@@ -10,6 +11,7 @@ export class StatusesService {
     @InjectRepository(Status)
     private statusRepository: Repository<Status>,
     private hallsService: HallsService,
+    @InjectRepository(Hall) private hallsRepo: Repository<Hall>,
   ) {}
 
   async findStatusByHallId(hall_id: number): Promise<Status> {
@@ -20,5 +22,15 @@ export class StatusesService {
   async findAllStatusesWithHalls(): Promise<Status[]> {
     const halls = await this.hallsService.findAll();
     return halls.map((hall) => hall.status);
+  }
+
+  async updateStatus(
+    hall_id: number,
+    newStatus: Partial<Status>, // Partial means all fields are marked as optional (?)
+  ): Promise<Status> {
+    const hall = await this.hallsService.findOne(hall_id);
+    hall.status = { ...hall.status, ...newStatus };
+    await this.hallsRepo.save(hall);
+    return hall.status;
   }
 }
